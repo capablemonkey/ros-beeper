f716c4b533a3be073d77dcbe377fc9bb5d71a14c
Gordon Zheng <gordon@greenhouse.io>
2017-12-26 16:41:23 -0500

    Play beep sound when non-zero velocity detected

--- main.py
+++ main.py
@@ -3,21 +3,39 @@
 import rospy
 from std_msgs.msg import String
 
+from geometry_msgs.msg import Twist
 from sound_play.libsoundplay import SoundClient
-from sound_play.msg import SoundRequest
 
 BEEP_SOUND_FILE = "/home/gordon/ros/beeper/bb8 - beep beep.wav"
 
 
+def absolute_sum(vector3):
+    return abs(vector3.x) + abs(vector3.y) + abs(vector3.z)
+
+
+def nonzero_velocity(twist_data):
+    return (absolute_sum(twist_data.linear) + absolute_sum(twist_data.angular)) > 0
+
+
+class Beeper:
+    def __init__(self, beep_sound_file):
+        self.sound_client = SoundClient()
+        self.beep = self.sound_client.waveSound(beep_sound_file)
+
+    def listen(self, data):
+        if nonzero_velocity(data):
+            rospy.loginfo("Motion detected; beeping!")
+            self.beep.play()
+
+
 def main():
     rospy.init_node('beeper')
-    soundhandle = SoundClient()
-    rospy.sleep(0.5)  # Ensure publisher connection is successful.
 
-    sound_beep = soundhandle.waveSound(BEEP_SOUND_FILE)
-    sound_beep.play()
+    beeper = Beeper(BEEP_SOUND_FILE)
+    rospy.Subscriber("cmd_vel", Twist, beeper.listen)
 
-    rospy.loginfo('Finished')
+    rospy.loginfo("beeper is now listening for nonzero velocity on /cmd_vel")
+    rospy.spin()
 
 
 if __name__ == '__main__':
